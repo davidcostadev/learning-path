a23ba597a74d1918e7c193210893bb39b351e0ae
David Costa <davidcostadev@gmail.com>
2020-02-15 21:04:40 -0300

    Initial Graphql Setup

--- graphql-essential-training/src/index.js
+++ graphql-essential-training/src/index.js
@@ -1,10 +1,25 @@
 const express = require('express');
+const graphqlHTTP = require('express-graphql');
+const schema = require('./schema');
 
 const app = express();
 
 app.get('/', (req, res) => {
   res.send('Hello!');
 });
+
+const root = {
+  hello: () => 'asdsdas'
+};
+
+app.use(
+  '/graphql',
+  graphqlHTTP({
+    schema,
+    rootValue: root,
+    graphiql: true
+  })
+);
 
 app.listen(3000, () => {
   console.log('Server is running');
