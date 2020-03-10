3e6cb547f7a3037ff0cf913d518af733ced70ed0
David Costa <davidcostadev@gmail.com>
2020-03-10 07:42:19 -0300

    Setting the subscription link

--- learning-apollo/client/src/App.tsx
+++ learning-apollo/client/src/App.tsx
@@ -1,25 +1,53 @@
 import React from 'react';
-import ApolloClient from 'apollo-boost';
+import { InMemoryCache } from 'apollo-cache-inmemory';
+// import ApolloClient from 'apollo-boost';
+import { ApolloClient } from 'apollo-client';
 import { ApolloProvider } from 'react-apollo';
 import { WebSocketLink } from 'apollo-link-ws';
+import { SubscriptionClient } from 'subscriptions-transport-ws';
+import { ApolloLink } from 'apollo-link';
+import { createHttpLink, HttpLink } from 'apollo-link-http';
 
 import './App.css';
 import AddContact from './components/AddContact';
 import Contacts from './components/Contacts';
 
-const wsLink = new WebSocketLink({
-  uri: `ws://localhost:4000/graphql`,
-  options: {
-    reconnect: true
-  }
+const GRAPHQL_ENDPOINT = 'ws://localhost:4000/graphql';
+
+// // const wsLink = new SubscriptionClient(GRAPHQL_ENDPOINT, {
+// //   reconnect: true
+// // });
+
+const link = ApolloLink.from([
+  // hasSubscriptionOperation,
+  // @ts-ignore
+  new WebSocketLink({
+    uri: GRAPHQL_ENDPOINT,
+    options: {
+      reconnect: true
+    }
+  }),
+  // @ts-ignore
+  createHttpLink({
+    uri: 'http://localhost:4000/graphql'
+  })
+]);
+
+// const link = new HttpLink({
+//   uri: 'http://localhost:4000/graphql'
+// });
+
+const client = new ApolloClient({
+  // @ts-ignore
+  link,
+  cache: new InMemoryCache()
 });
-const client = new ApolloClient({ uri: 'http://localhost:4000/graphql' });
 
 function App() {
   return (
     <ApolloProvider client={client}>
-      <div className='App'>
-        <header className='App-header'>
+      <div className="App">
+        <header className="App-header">
           <h1>CRM</h1>
         </header>
         <div>
